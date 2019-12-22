adf26f3a986a813d66c1fb5034f075a93d923857
Towfiqul Islam <towfiqu@gmail.com>
2019-12-22 18:32:13 +0600

    anime plugin added

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -155,7 +155,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7110" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13238" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
